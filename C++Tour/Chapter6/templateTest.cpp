31e4ee759b53d6ed23a73b076e7ba8c58163534a
TheDigar <edgar.gadbem@gmail.com>
2021-05-12 19:11:26 -0300

    Test create vectors of int and double

--- C++Tour/Chapter6/templateTest.cpp
+++ C++Tour/Chapter6/templateTest.cpp
@@ -1,11 +1,12 @@
+#include <random>
+#include <iostream>
+
 #include <MyVectorTemplate.h>
 
 int main()
 {
     MyVector<int> vi {10};
-    MyVector<double> vd{11};
-
-    
+    MyVector<double> vd{10};
 
     return 0;
 }
